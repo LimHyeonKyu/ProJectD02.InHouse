4a1e30c8d084cafd4312f05cd39aac01e8612c22
LimHyeonKyu <dqntm123@naver.com>
2018-05-26 15:08:49 +0900

    20180526
    오후작업시작

--- ProjectD02/Assets/Scripts/lobby/UpDownBtn.cs
+++ ProjectD02/Assets/Scripts/lobby/UpDownBtn.cs
@@ -7,42 +7,42 @@ using UnityEngine.UI;
 
 public class UpDownBtn : MonoBehaviour {
 
-    public UILabel rfuILabel;//강화수치 라벨
+    public UILabel[] rfuILabel;//강화수치 라벨
+    public ButtonManager btmMg;
 
     private void Start()
     {
-        
+        btmMg= GameObject.Find("BtnManager").GetComponent<ButtonManager>();
     }
 
     void Update()
     {
-        SetNum();
+        
     }
     public void UpBtn()
     {
         EffectSoundManager.iNstance.audios.clip = EffectSoundManager.iNstance.effectClip[0];
         EffectSoundManager.iNstance.audios.PlayOneShot(EffectSoundManager.iNstance.audios.clip);
-        if(LevelManager.instanCe.lv[0] <= 10)
+        for (int i = 0; i < LevelManager.instanCe.lv.Length; i++)
         {
-            if (MoneyManager.inStance.reinFoceValue<=MoneyManager.inStance.goldCount && MoneyManager.inStance.goldCount>=0)
-            //강화값이 MoneyManager 싱글톤의 골드값보다 작거나 같은경우와 MoneyManager 싱글톤 골드값이 0보다 크거나 같을경우 강화시킨다
-            {
-                MoneyManager.inStance.goldCount -= MoneyManager.inStance.reinFoceValue;
-                MoneyManager.inStance.reinFoceValue *= 2;
-                LevelManager.instanCe.lv[0] += 1;
+            if(btmMg.target==btmMg.buttons[i])
+            { 
+                if (LevelManager.instanCe.lv[i] <= 10)
+                {
+                    if (MoneyManager.inStance.reinFoceValue <= MoneyManager.inStance.goldCount && MoneyManager.inStance.goldCount >= 0)
+                    //강화값이 MoneyManager 싱글톤의 골드값보다 작거나 같은경우와 MoneyManager 싱글톤 골드값이 0보다 크거나 같을경우 강화시킨다
+                    {
+                        MoneyManager.inStance.goldCount -= MoneyManager.inStance.reinFoceValue;
+                        MoneyManager.inStance.reinFoceValue *= 2;
+                        LevelManager.instanCe.lv[i] += 1;
+                    }
+                }
             }
-        }
-        if (LevelManager.instanCe.lv[0] >= 10)
-        {
-            LevelManager.instanCe.lv[0] = 10;
-        }
-    }
-
-    void SetNum()
-    {
-        if (rfuILabel != null)
-        {
-            rfuILabel.text = Convert.ToString("LV "+ LevelManager.instanCe.lv[0]);     //업,다운버튼을 누를때 바뀐 레벨을 스트링으로 라벨에 넣어줌
+            if (LevelManager.instanCe.lv[i] >= 10)
+            {
+                LevelManager.instanCe.lv[i] = 10;
+            }
+            rfuILabel[i].text = Convert.ToString("LV " + LevelManager.instanCe.lv[i]);
         }
     }
 }
